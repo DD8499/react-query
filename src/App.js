4c296484271bd965a1626852aae4f69086bdfa40
Bhoomi <bhoomi.p@tridhya.com>
2023-03-07 09:50:42 +0530

    react-query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useQuery} from 'react-query';
 import './App.css';
 
 function App() {
+  const queryData = useQuery({
+    queryKey: ['firstQuery'],  //it should be unique for all queries
+    queryFn: async () => {
+      const response = await axios.get('https://dummyjson.com/products')
+      return response
+    },
+    // enabled: false
+    staleTime: 1000
+  })
+
+  let { data, isLoading, isError } = queryData
+  if (isLoading) return <h1>Loading....</h1>
+  if (isError) return <h1>'Some Thing Went Wrong !!'</h1>
+
+  console.log(data?.data?.products, 'data');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React-Query
       </header>
+      {data?.data?.products.map((product) => {
+        return <div key={product.id}>
+          {product?.title}
+        </div>
+      })}
     </div>
   );
 }
